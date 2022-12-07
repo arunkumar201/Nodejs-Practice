d78043c462f7197e2b1b7491f7558e3c249980b8
arungit2020 <aruprajapati9825@gmail.com>
2022-12-07 23:03:45 +0530

    Intial Commit

---
+++ Server/index.js
@@ -0,0 +1,13 @@
+let getData=()=>{ 
+return "My Name is Arun Kumar"
+}
+let a = "Hello World";
+console.log(a);
+let arr = [1, 2, 3, 4, 4, 5];
+arr.map((i) => {
+  console.log(i);
+});
+console.log("Hi I am Arun Kumar!");
+
+
+console.log(getData());
